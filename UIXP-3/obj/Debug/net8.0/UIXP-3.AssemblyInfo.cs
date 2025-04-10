67e82d3eb8281d2f23f0205b83e633f67a5c2070
Nafim <M.R.NAFIM-2022@hull.ac.uk>
2025-04-10 01:34:43 +0100

    Added RZ pages

--- UIXP-3/obj/Debug/net8.0/UIXP-3.AssemblyInfo.cs
+++ UIXP-3/obj/Debug/net8.0/UIXP-3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UIXP-3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+348fb3761887142b5789b7c672ec12483a9301b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1755bbdf32b6cf50b5a6b99b37e64a78b564c11")]
 [assembly: System.Reflection.AssemblyProductAttribute("UIXP-3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UIXP-3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
