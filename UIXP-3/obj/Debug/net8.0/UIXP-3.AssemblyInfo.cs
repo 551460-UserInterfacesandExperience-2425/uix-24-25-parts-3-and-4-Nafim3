62c35ef259584ea0b414b21744e51defd4eb5b13
Nafim <M.R.NAFIM-2022@hull.ac.uk>
2025-04-28 14:25:56 +0100

    changed

--- UIXP-3/obj/Debug/net8.0/UIXP-3.AssemblyInfo.cs
+++ UIXP-3/obj/Debug/net8.0/UIXP-3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UIXP-3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+153c53d096744ea076c21a46f8e43c2a845073f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+595cd75f835e4f044616ed0cda6bf06845ab9aad")]
 [assembly: System.Reflection.AssemblyProductAttribute("UIXP-3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UIXP-3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
